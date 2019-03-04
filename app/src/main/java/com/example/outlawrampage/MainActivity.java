03b8bb64134115efa23bc752c9df6bcd3a25f54f
Emily <eamarche@mtu.edu>
2019-03-04 16:43:06 -0500

    Test commit from android studio

---
+++ app/src/main/java/com/example/outlawrampage/MainActivity.java
@@ -0,0 +1,167 @@
+package com.example.outlawrampage;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageButton right;
+    private ImageButton left;
+    private ImageButton jump;
+    private ImageView runner;
+    private Handler handler = new Handler();
+    private Timer timer;
+
+//    public ImageView getRunner() {
+//        return runner;
+//    }
+//
+//    public Button getRight() {
+//        return right;
+//    }
+//
+//    public Button getLeft() {
+//        return left;
+//    }
+//
+//    public Button getJump() {
+//        return jump;
+//    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        runner = findViewById(R.id.runner);
+
+        right = findViewById(R.id.right);
+        right.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                //citation: https://www.youtube.com/watch?v=06rVQ1kUXc4
+                switch (event.getAction()) {
+
+                    case MotionEvent.ACTION_DOWN:
+
+                        //citation: https://www.youtube.com/watch?v=UxbJKNjQWD8
+                        timer = new Timer(); //need to start a new timer every time you press the button
+                        timer.schedule(new TimerTask() {
+                            @Override
+                            public void run() {
+                                handler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        runner.setX(runner.getX() + 5);
+                                    }
+                                });
+                            }
+                        }, 1, 50);
+                        return true;
+
+                    case MotionEvent.ACTION_UP:
+                        timer.cancel(); //end the timer when you release the button
+                        return true;
+                }
+
+                return false;
+            }
+        });
+
+
+        left = findViewById(R.id.left);
+        left.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                //citation: https://www.youtube.com/watch?v=06rVQ1kUXc4
+                switch (event.getAction()) {
+
+                    case MotionEvent.ACTION_DOWN:
+
+                        //citation: https://www.youtube.com/watch?v=UxbJKNjQWD8
+                        timer = new Timer(); //need to start a new timer every time you press the button
+                        timer.schedule(new TimerTask() {
+                            @Override
+                            public void run() {
+                                handler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        runner.setX(runner.getX() - 5);
+                                    }
+                                });
+                            }
+                        }, 1, 50);
+                        return true;
+
+                    case MotionEvent.ACTION_UP:
+                        timer.cancel(); //end the timer when you release the button
+                        return true;
+                }
+
+                return false;
+            }
+        });
+
+
+        jump = findViewById(R.id.jump);
+        jump.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                //citation: https://www.youtube.com/watch?v=06rVQ1kUXc4
+                switch (event.getAction()) {
+
+                    case MotionEvent.ACTION_DOWN:
+
+                        final long startTime = System.currentTimeMillis();
+
+                        //citation: https://www.youtube.com/watch?v=UxbJKNjQWD8
+                        timer = new Timer(); //need to start a new timer every time you press the button
+                        timer.schedule(new TimerTask() {
+                            @Override
+                            public void run() {
+                                for (int i = 0; i < 10; i++) {
+                                    //citation: https://www.youtube.com/watch?v=PGMrMZLNhUk
+                                    handler.postDelayed(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            runner.setY(runner.getY() - 2);
+                                        }
+                                    }, 500);
+                                }
+                                if (System.currentTimeMillis() >= startTime + 500) {
+                                    timer.cancel();
+                                }
+                                for (int i = 0; i < 10; i++) {
+                                    handler.postDelayed(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            runner.setY(runner.getY() + 2);
+                                        }
+                                    }, 1000);
+                                }
+                            }
+                        }, 1, 50);
+                        return true;
+
+                    case MotionEvent.ACTION_UP:
+                        timer.cancel(); //end the timer when you release the button
+                        return true;
+                }
+
+                return false;
+            }
+        });
+    }
+}
+
---
+++ app/src/test/java/com/example/outlawrampage/ExampleUnitTest.java
@@ -0,0 +1,42 @@
+package com.example.outlawrampage;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+
+//    @Test
+//    public void getRightTest () {
+//        MainActivity main = new MainActivity();
+//
+//        assertEquals(main.right, main.getRight());
+//    }
+//
+//    @Test
+//    public void getLeftTest () {
+//        MainActivity main = new MainActivity();
+//
+//        assertEquals(main.left, main.getLeft());
+//    }
+//
+//    @Test
+//    public void getJumpTest () {
+//        MainActivity main = new MainActivity();
+//
+//        assertEquals(main.jump, main.getJump());
+//    }
+//
+//    @Test
+//    public void getRunnerTest () {
+//        MainActivity main = new MainActivity();
+//
+//        assertEquals(main.runner, main.getRunner());
+//    }
+
+}
